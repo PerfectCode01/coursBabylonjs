94303712b1d20c17d859b25d22cfa92d4d4ae4c9
baz3004 <japhetbazungula01@gmail.com>
2024-04-23 19:27:49 -1100

    creation du projet

---
+++ src/Experiences/Experience1.ts
@@ -0,0 +1,9 @@
+import { Engine } from "@babylonjs/core";
+
+export class ChutteLibre {
+    engine : Engine;
+    scene : Scene;
+    constructor(private canvas:HTMLCanvasElement){
+        this.engine = new
+    }
+}
